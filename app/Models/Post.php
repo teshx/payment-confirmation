08806bf9cbd281a77736dcd44f5949f9d4d37102
teshager habtie <teshie00001@gmail.com>
2025-03-16 14:12:30 -0500

    creating post nodel

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+
+
+
+
+
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+}
